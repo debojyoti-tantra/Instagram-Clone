ed35fa31860273331b456dfeed01d6ee39f7948a
Debojyoti Tantra <tantradebojyoti@gmail.com>
2025-02-09 15:35:51 +0530

    Update Post.jsx

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -51,7 +51,7 @@ export default function Post({post}) {
    const likeDislikeHandler = async () => {
       try {
          const action = liked ? 'dislike' : 'like';
-         const res = await axios.get(`/api/v1/post/${post._id}/${action}`, {withCredentials:true});
+         const res = await axios.get(`https://instadebo.onrender.com/api/v1/post/${post._id}/${action}`, {withCredentials:true});
          if (res.data.success) {
             const updatedLikes = liked ? postLike-1 : postLike+1;
             setPostLike(updatedLikes);
@@ -73,7 +73,7 @@ export default function Post({post}) {
    
    const commentHandler = async () => {
       try {
-         const res = await axios.post(`/api/v1/post/${post._id}/comment`, {text}, {
+         const res = await axios.post(`https://instadebo.onrender.com/api/v1/post/${post._id}/comment`, {text}, {
             headers: {
                'Content-Type': 'application/json'
             },
@@ -100,7 +100,7 @@ export default function Post({post}) {
    const deletePostHandler = async () => {
       try {
          setLoading(true);
-         const res = await axios.delete(`/api/v1/post/delete/${post?._id}`, {withCredentials:true});
+         const res = await axios.delete(`https://instadebo.onrender.com/api/v1/post/delete/${post?._id}`, {withCredentials:true});
          if (res.data.success) {
             const updatedPostData = posts.filter((postItem) => postItem?._id !== post?._id)
             dispatch(setPosts(updatedPostData));
@@ -116,7 +116,7 @@ export default function Post({post}) {
    
    const bookmarkHandler = async () => {
       try {
-         const res = await axios.get(`/api/v1/post/${post._id}/bookmark`, {withCredentials:true});
+         const res = await axios.get(`https://instadebo.onrender.com/api/v1/post/${post._id}/bookmark`, {withCredentials:true});
          console.log(res);
          if (res.data.success) {
             toast.success(res.data.message);
