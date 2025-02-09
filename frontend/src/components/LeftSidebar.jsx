9468b6937e3cb1b0a7568373ac862c7ab10200f8
Debojyoti Tantra <tantradebojyoti@gmail.com>
2025-02-09 15:34:21 +0530

    Update LeftSidebar.jsx

--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -34,7 +34,7 @@ export default function LeftSidebar() {
    
    const logoutHandler = async () => {
       try {
-         const res = await axios.get('/api/v1/user/logout', {withCredentials:true});
+         const res = await axios.get('https://instadebo.onrender.com/api/v1/user/logout', {withCredentials:true});
          if (res.data.success) {
             dispatch(setLikeNotification([]));
             dispatch(setAuthUser(res.data.user));
